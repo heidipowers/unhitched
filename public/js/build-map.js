fa16f9d6a998287a7177f77f84dd36cb9c80fe67
Heidi Powers <heidipowers@gmail.com>
2016-08-13 20:20:03 -0400

    background animations

--- public/js/build-map.js
+++ public/js/build-map.js
@@ -1,75 +1,75 @@
-'use strict'
+// 'use strict'
 
-function getThatMap() {
+// function getThatMap() {
 
-  console.log('map loaded')
-
-
-
-  const drawMap = function(points){
-    // Create the map
-    const mapOptions = {
-        scrollwheel: false,
-        styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
-
-    };
-    const incidentMap = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    const bounds = new google.maps.LatLngBounds()
-
-    // Create information Window
-    const infoWindow = new google.maps.InfoWindow();
-
-    points.forEach(function(point){
-
-      // Display multiple markers on a map based on DB info
-      const {incident_lat:lat,incident_lng:lng, incident_location:title} = point
-
-      const infoWindowContent =
-        '<div class="info_content">' +
-        '<h3><span class="infostart">Incident Type:</span> '+ point.incident_type + '</h3>' +
-        '<h3><span class="infostart">Date of Incident:</span> '+ point.incident_date + '</h3>' +
-        '<h3><span class="infostart">Horse Name:</span> '+ point.horse_name + '</h3>' +
-        '<p>' + point.incident_desc + '</p>' + '</div>';
-
-      const marker = new google.maps.Marker({
-        position: new google.maps.LatLng(Number.parseFloat(lat), Number.parseFloat(lng)),
-        map: incidentMap,
-        title: title
-      });
-
-
-      // Allow each marker to have an info window based on DB info
-      const id =  point.incident_id;
-      google.maps.event.addListener(marker, 'click', (function(marker, id) {
-
-            return function() {
-                infoWindow.setContent(infoWindowContent);
-                infoWindow.open(incidentMap, marker);
-            }
-        })(marker,id));
+//   console.log('map loaded')
 
 
 
-      bounds.extend(marker.position)
+//   const drawMap = function(points){
+//     // Create the map
+//     const mapOptions = {
+//         scrollwheel: false,
+//         styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
 
-    });//end forEach
+//     };
+//     const incidentMap = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+//     const bounds = new google.maps.LatLngBounds()
+
+//     // Create information Window
+//     const infoWindow = new google.maps.InfoWindow();
+
+//     points.forEach(function(point){
+
+//       // Display multiple markers on a map based on DB info
+//       const {incident_lat:lat,incident_lng:lng, incident_location:title} = point
+
+//       const infoWindowContent =
+//         '<div class="info_content">' +
+//         '<h3><span class="infostart">Incident Type:</span> '+ point.incident_type + '</h3>' +
+//         '<h3><span class="infostart">Date of Incident:</span> '+ point.incident_date + '</h3>' +
+//         '<h3><span class="infostart">Horse Name:</span> '+ point.horse_name + '</h3>' +
+//         '<p>' + point.incident_desc + '</p>' + '</div>';
+
+//       const marker = new google.maps.Marker({
+//         position: new google.maps.LatLng(Number.parseFloat(lat), Number.parseFloat(lng)),
+//         map: incidentMap,
+//         title: title
+//       });
 
 
-    //Readjust the map center and zoom
-    incidentMap.fitBounds(bounds)
+//       // Allow each marker to have an info window based on DB info
+//       const id =  point.incident_id;
+//       google.maps.event.addListener(marker, 'click', (function(marker, id) {
 
-    const boundsListener = google.maps.event.addListener((incidentMap), 'bounds_changed', function(event) {
-        this.setZoom(13);
-        google.maps.event.removeListener(boundsListener);
-    });
-  }
+//             return function() {
+//                 infoWindow.setContent(infoWindowContent);
+//                 infoWindow.open(incidentMap, marker);
+//             }
+//         })(marker,id));
 
 
-//Go get the data
-  $.get('/incident')
-    .then(drawMap)
 
-}
+//       bounds.extend(marker.position)
+
+//     });//end forEach
+
+
+//     //Readjust the map center and zoom
+//     incidentMap.fitBounds(bounds)
+
+//     const boundsListener = google.maps.event.addListener((incidentMap), 'bounds_changed', function(event) {
+//         this.setZoom(13);
+//         google.maps.event.removeListener(boundsListener);
+//     });
+//   }
+
+
+// //Go get the data
+//   $.get('/incident')
+//     .then(drawMap)
+
+// }
 
 
 
