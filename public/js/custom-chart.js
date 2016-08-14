5960c0f8ea187e978368844a125b7f8d89647bce
Heidi Powers <heidipowers@gmail.com>
2016-08-13 21:57:09 -0400

    Create default chart view and move scene 2 to partial

--- public/js/custom-chart.js
+++ public/js/custom-chart.js
@@ -1,9 +1,6 @@
 'use strict'
 
-
-
-
-function buildMap(){
+function buildChart(){
 
 
 
@@ -14,16 +11,11 @@ function buildMap(){
               let park = 0;
               let collapse = 0;
               let total = incidents.length;
-              let fatalities = 0;
-              let yearDied = [];
               let years = [];
-              let names = [];
-              let month = [];
 
               //GET DATA FOR VARIABLES
               incidents.forEach(function(incident){
                 years.push(incident.incident_year);
-                month.push(incident.incident_month);
 
                 if (incident.incident_type === 'Traffic'){
                   traffic+=1;
@@ -33,39 +25,7 @@ function buildMap(){
                   park+=1;
                 }
 
-                if (incident.fatal){
-                  fatalities+=1;
-                  yearDied.push(incident.incident_year);
-                  names.push(incident.horse_name);
-
-                }
-
               })//end forEach
-
-              //thank you stackOverflow: http://stackoverflow.com/questions/9229645/remove-duplicates-from-javascript-array
-              let cleanYears = [ ...new Set(years) ]
-              //yearDied = [ ...new Set(yearDied)]
-              yearDied.reverse();
-
-
-              //Make each name individual for chart x axis
-              names.reverse();
-              let nameAdjust = 0;
-                for (let i = 0; i < yearDied.length; i++){
-                  if (names[i] === 'Unknown') {
-                    nameAdjust+=1;
-                    names[i] = (names[i] + ' Horse ' + nameAdjust);
-                  }
-                }
-
-
-
-              //Collect the number of incidents per month
-              let months = {};
-              for(let i = 0; i< month.length; i++) {
-                 let count = month[i];
-                 months[count] = months[count] ? months[count]+1 : 1;
-              }
 
 
                //Collect the number of incidents per year
@@ -81,17 +41,10 @@ function buildMap(){
 
               for (var year in yearsCollect) {
                 if (yearsCollect.hasOwnProperty(year)) {
-                  console.log(year + " -> " + yearsCollect[year]);
                   yearNum.push(year);
                   yearCount.push(yearsCollect[year])
                 }
               }
-
-              console.log(yearNum)
-              console.log(yearCount)
-
-
-
 
 
               //SET CHART VARIABLES
@@ -108,7 +61,6 @@ function buildMap(){
                //GRAPH CHOICE
 
                  let graphInput = $userChoice.val();
-                 console.log(graphInput)
                   if (graphInput === 'type'){
 
                       type = 'pie';
@@ -134,10 +86,8 @@ function buildMap(){
                       };
 
 
-                  } else if (graphInput === 'date'){
+                  } else {
                       type = 'line';
-                      // graphLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-                      // graphData = cleanYears;
 
                       graphLabels = yearNum;
                       graphData = yearCount;
@@ -170,40 +120,7 @@ function buildMap(){
                             }
                         ]
                     };
-                  } else {
-                      type = 'line';
-                      graphLabels = names;
-                      graphData = yearDied;
-
-                      data =  {
-                        labels: graphLabels,
-                        datasets: [
-                            {
-                                label: "Year",
-                                fill: false,
-                                lineTension: 0.1,
-                                backgroundColor: "rgba(75,192,192,0.4)",
-                                borderColor: "rgba(75,192,192,1)",
-                                borderCapStyle: 'butt',
-                                borderDash: [],
-                                borderDashOffset: 0.0,
-                                borderJoinStyle: 'miter',
-                                pointBorderColor: "rgba(75,192,192,1)",
-                                pointBackgroundColor: "#fff",
-                                pointBorderWidth: 1,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                                pointHoverBorderColor: "rgba(220,220,220,1)",
-                                pointHoverBorderWidth: 2,
-                                pointRadius: 1,
-                                pointHitRadius: 10,
-                                data: graphData,
-                                spanGaps: false,
-                            }
-                        ]
-                    };
                   }
-
               //BUILD CHART
               const ctx = document.getElementById("incidentChart");
 
@@ -219,9 +136,9 @@ function buildMap(){
 }//end buildmap
 
  const $userChoice = $('#chart-select');
- $userChoice.on('change', buildMap);
+ $userChoice.on('change', buildChart);
 
-//buildMap();
+buildChart();
 
 
 
