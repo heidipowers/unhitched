fa8e84bf845664f93625014e16430d8607bed622
Heidi Powers <heidipowers@gmail.com>
2016-08-16 10:14:12 -0400

    Fix Chart

--- public/js/custom-chart.js
+++ public/js/custom-chart.js
@@ -1,23 +1,19 @@
 'use strict'
 
-function buildChart(){
 
-let colorbright = "#EE6C4D";
-let colordark = "#D3D0CB";
-let colormed = "#6E8898";
-let colorlight = "#2E5266";
 
-          console.log('fired')
 
-            $.get('/incident', function(incidents) {
-              /*optional stuff to do after success */
-              //DATA RETURN FOR TYPE
+
+function buildChart(incidents){
+
               let traffic = 0;
               let fatalities = 0;
               let park = 0;
               let collapse = 0;
               let total = incidents.length;
               let years = [];
+
+
 
               //GET DATA FOR VARIABLES
               incidents.forEach(function(incident){
@@ -36,7 +32,6 @@ let colorlight = "#2E5266";
                 }
 
               })//end forEach
-
 
                //Collect the number of incidents per year
               let yearsCollect = {};
@@ -77,8 +72,13 @@ let colorlight = "#2E5266";
                let graphData = defaultData;
                let type = defaultType;
 
+
                //GRAPH CHOICE
                //https://coolors.co
+              let colorbright = "#EE6C4D";
+              let colordark = "#D3D0CB";
+              let colormed = "#6E8898";
+              let colorlight = "#2E5266";
 
                  let graphInput = $userChoice.val();
                   if (graphInput === 'type'){
@@ -143,177 +143,46 @@ let colorlight = "#2E5266";
                         ]
                     };
                   }
+
               //BUILD CHART
 
 
+              //DIV THAT HOLDS THE CANVAS
+              const chartDiv = $('.chart');
 
-              const ctx = document.getElementById("incidentChart").getContext("2d");
-              const incidentChart = new Chart(ctx, {
+              //EMPTY THE CHART DIV OF ANY CANVAS
+              chartDiv.empty();
+
+              //CREATE THE CANVAS AND APPEND TO THE CHART DIV
+              const canvas = $('<canvas id="incidentChart">');
+              chartDiv.append(canvas);
+
+              //DECLARE CANVAS
+              const chart = document.getElementById("incidentChart").getContext("2d");
+
+
+
+                const incidentChart = new Chart(chart, {
                 type: type,
                 data: data,
                 options: options
               });//end chart
 
-        });//end get
+
 
 }//end buildmap
 
 //initial chartBuild on page load
-buildChart();
-
+//buildChart();
+$.get('/incident')
+  .then(buildChart)
 //update Chart on userChoice
 const $userChoice = $('#chart-select');
- $userChoice.on('change', buildChart);
+ $userChoice.on('change', function(){
+    $.get('/incident')
+      .then(buildChart)
+ });
 
-
-
-// function buildCharts(){
-
-
-
-//           console.log('fired')
-
-//             $.get('/incident', function(incidents) {
-//               /*optional stuff to do after success */
-//               //DATA RETURN FOR TYPE
-//               let traffic = 0;
-//               let fatalities = 0;
-//               let park = 0;
-//               let collapse = 0;
-//               let total = incidents.length;
-//               let years = [];
-
-//               //GET DATA FOR VARIABLES
-//               incidents.forEach(function(incident){
-//                 years.push(incident.incident_year);
-
-//                 if ( incident.incident_type === 'Traffic'){
-//                   traffic+=1;
-//                 } else if ( incident.incident_type === 'Collapse'){
-//                   collapse+=1;
-//                 } else {
-//                   park+=1;
-//                 }
-
-//                 if ( incident.fatal ) {
-//                   fatalities+=1;
-//                 }
-
-//               })//end forEach
-
-
-//                //Collect the number of incidents per year
-//               let yearsCollect = {};
-//               for(let i = 0; i< years.length; i++) {
-//                  //let count = years[i];
-//                  yearsCollect[years[i]] = yearsCollect[years[i]] ? yearsCollect[years[i]]+1 : 1;
-
-//               }
-
-//               //Make yearsCollect into 2 arrays
-//               let yearNum = [];
-//               let yearCount = [];
-
-//               for (var year in yearsCollect) {
-//                 if (yearsCollect.hasOwnProperty(year)) {
-//                   yearNum.push(year);
-//                   yearCount.push(yearsCollect[year])
-//                 }
-//               }
-
-
-//               //SET CHART VARIABLES
-
-//                let data;
-//                let options;
-//                let pieoptions;
-//                let lineoptions;
-
-
-
-//                //GRAPH CHOICE
-
-
-
-
-//                       let piegraphLabels = ["Traffic Incidents", "Horse Collapses", "Incident Inside Central Park", "Horse Fatality"];
-//                       let piegraphData = [traffic, collapse, park, fatalities];
-
-//                       let piedata = {
-//                           labels: piegraphLabels,
-//                           datasets: [
-//                               {
-//                                   data: piegraphData,
-//                                   backgroundColor: [
-//                                       "#419ea8",
-//                                       "#1F1F1F",
-//                                       "#3D3D3D",
-//                                       "#6f0d0d"
-//                                   ],
-//                                   hoverBackgroundColor: [
-//                                       "#FF6384",
-//                                       "#36A2EB",
-//                                       "#FFCE56",
-//                                       "#3D3D3D"
-//                                   ]
-//                               }]
-//                       };
-
-
-
-//                       let linegraphLabels = yearNum;
-//                       let linegraphData = yearCount;
-
-//                       let linedata =  {
-//                         labels: linegraphLabels,
-//                         datasets: [
-//                             {
-//                                 label: "NYC Carriage Incidents by Year",
-//                                 fill: false,
-//                                 lineTension: 0.1,
-//                                 backgroundColor: "rgba(75,192,192,0.4)",
-//                                 borderColor: "rgba(75,192,192,1)",
-//                                 borderCapStyle: 'butt',
-//                                 borderDash: [],
-//                                 borderDashOffset: 0.0,
-//                                 borderJoinStyle: 'miter',
-//                                 pointBorderColor: "rgba(75,192,192,1)",
-//                                 pointBackgroundColor: "#fff",
-//                                 pointBorderWidth: 1,
-//                                 pointHoverRadius: 5,
-//                                 pointHoverBackgroundColor: "rgba(75,192,192,1)",
-//                                 pointHoverBorderColor: "rgba(220,220,220,1)",
-//                                 pointHoverBorderWidth: 2,
-//                                 pointRadius: 1,
-//                                 pointHitRadius: 10,
-//                                 data: linegraphData,
-//                                 spanGaps: false,
-//                             }
-//                         ]
-//                     };
-
-//               //BUILD CHART
-
-//                const pieChart = document.getElementById("incidentPieChart");
-//                const incidentPieChart = new Chart(pieChart, {
-//                 type: 'pie',
-//                 data: piedata,
-//                 options: pieoptions
-//                });
-
-//               const lineChart = document.getElementById("incidentLineChart");
-//               const incidentLineChart = new Chart(lineChart, {
-//                 type: 'line',
-//                 data: linedata,
-//                 options: lineoptions
-//               });//end chart
-
-//         });//end get
-
-// }//end buildmap
-
-// //initial chartBuild on page load
-// buildCharts();
 
 
 
